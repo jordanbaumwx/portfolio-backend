fc1bb3726b32b1a8653071f30626318c9961f0c3
Jordan Baumgardner <developer@jordanbaumgardner.com>
2023-01-15 14:20:38 -0500

    Setup routes for modularity.

---
+++ src/api/rest/routes/experiences/index.ts
@@ -0,0 +1,28 @@
+// Description: Profile routes
+
+import { PrismaClient } from '@prisma/client';
+import { Router, Request, Response } from 'express';
+import { asyncHandler } from '../../utils';
+
+const router = Router();
+
+router.get('/:skillId', async (req: Request, res: Response) => {
+  const skillId = req.params.skillId;
+  // Make sure skillId is defined and a number
+  if (!skillId || typeof skillId !== 'number') {
+    return res.status(400).send('Skill ID is required');
+  }
+
+  const prisma = new PrismaClient();
+  const experiences = await prisma.skillsOnExperiences.findMany({
+    where: {
+      skillId,
+    },
+    include: {
+      experience: true,
+    },
+  });
+  return res.send(experiences);
+});
+
+export default router;
---
+++ src/api/rest/routes/skills/index.ts
@@ -0,0 +1,30 @@
+// Description: Profile routes
+
+import { PrismaClient } from '@prisma/client';
+import { Router, Request, Response } from 'express';
+import { asyncHandler } from '../../utils';
+
+const router = Router();
+router.get(
+  '/:experienceId',
+  asyncHandler(async (req: Request, res: Response) => {
+    const experienceId = req.params.experienceId;
+    // Make sure experienceId is defined and a number
+    if (!experienceId || typeof experienceId !== 'number') {
+      return res.status(400).send('Experience ID is required');
+    }
+
+    const prisma = new PrismaClient();
+    const skills = await prisma.skillsOnExperiences.findMany({
+      where: {
+        experienceId,
+      },
+      include: {
+        skill: true,
+      },
+    });
+    return res.send(skills);
+  })
+);
+
+export default router;
